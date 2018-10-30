4984e256d282e47f0b2a773745c1855fa61907c4
mafioznik77 <narek19992009@yandex.ru>
2018-10-30 23:19:32 +0300

    Tset add

--- src/tset.cpp
+++ src/tset.cpp
@@ -2,46 +2,50 @@
 //
 // tset.cpp - Copyright (c) Гергель В.П. 04.10.2001
 //   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (19.04.2015)
-//
+// Работу над шаблонами выполнил студент группы 381706-4 Серобян Нарек.
 // Множество - реализация через битовые поля
 /*
 #include "tset.h"
 
-TSet::TSet(int mp) : BitField(-1)
+TSet::TSet(int mp) : BitField(mp), MaxPower(mp)
 {
 }
 
 // конструктор копирования
-TSet::TSet(const TSet &s) : BitField(-1)
+TSet::TSet(const TSet &s) : BitField(s.BitField), MaxPower(s.MaxPower)
 {
 }
 
 // конструктор преобразования типа
-TSet::TSet(const TBitField &bf) : BitField(-1)
+TSet::TSet(const TBitField &bf) : BitField(bf), MaxPower( bf.GetLength() )
 {
+
 }
 
 TSet::operator TBitField()
 {
+return BitField;
 }
 
 int TSet::GetMaxPower(void) const // получить макс. к-во эл-тов
 {
+return MaxPower;
 }
 
 int TSet::IsMember(const int Elem) const // элемент множества?
 {
-    return 0;
+return BitField.GetBit(Elem);
 }
 
 void TSet::InsElem(const int Elem) // включение элемента множества
 {
+BitField.SetBit(Elem);
 }
 
 void TSet::DelElem(const int Elem) // исключение элемента множества
 {
+BitField.ClrBit(Elem);
 }
-
 // теоретико-множественные операции
 
 TSet& TSet::operator=(const TSet &s) // присваивание

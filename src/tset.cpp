1f2231fd1be3e6833b98621eb01af7210b209a92
mafioznik77 <narek19992009@yandex.ru>
2018-11-03 20:35:42 +0300

    tset add

--- src/tset.cpp
+++ src/tset.cpp
@@ -20,31 +20,33 @@ TSet::TSet(const TSet &s) : BitField(s.BitField), MaxPower(s.MaxPower)
 TSet::TSet(const TBitField &bf) : BitField(bf), MaxPower( bf.GetLength() )
 {
 
+
+
 }
 
 TSet::operator TBitField()
 {
-return BitField;
+	return BitField;
 }
 
 int TSet::GetMaxPower(void) const // получить макс. к-во эл-тов
 {
-return MaxPower;
+	return MaxPower;
 }
 
 int TSet::IsMember(const int Elem) const // элемент множества?
 {
-return BitField.GetBit(Elem);
+	return BitField.GetBit(Elem);
 }
 
 void TSet::InsElem(const int Elem) // включение элемента множества
 {
-BitField.SetBit(Elem);
+	BitField.SetBit(Elem);
 }
 
 void TSet::DelElem(const int Elem) // исключение элемента множества
 {
-BitField.ClrBit(Elem);
+	BitField.ClrBit(Elem);
 }
 // теоретико-множественные операции
 
@@ -78,30 +80,52 @@ TSet TSet::operator+(const TSet &s) // объединение
 
 TSet TSet::operator+(const int Elem) // объединение с элементом
 {
-	return (BitField|Elem);
+	TSet res(*this);
+	res.InsElem(Elem);
+	return res;
 }
 
 TSet TSet::operator-(const int Elem) // разность с элементом
 {
-	return (BitField&Elem);
+	TSet res(*this);
+	res.DelElem(Elem);
+	return res;
 }
 
 TSet TSet::operator*(const TSet &s) // пересечение
 {
-	return (BitField&s.BitField);
+	TSet temp(BitField & s.BitField);
+	return temp;
 }
 
 TSet TSet::operator~(void) // дополнение
 {
-	return (~BitField);
+	TSet temp(~BitField);
+	return temp;
 }
 
 // перегрузка ввода/вывода
 
 istream &operator>>(istream &istr, TSet &s) // ввод
 {
+	int Elem;
+	istr >> Elem;
+	while (Elem >= 0 && Elem < s.MaxPower)
+	{
+		s.InsElem(Elem);
+		istr >> Elem;
+	}
+	return istr;
 }
 
 ostream& operator<<(ostream &ostr, const TSet &s) // вывод
 {
+	ostr << "{ ";
+	for (int i = 0; i < s.MaxPower; i++)
+	{
+		if (s.IsMember(i))
+			ostr << i << ' ';
+	}
+	ostr << " }";
+	return ostr;
 }
